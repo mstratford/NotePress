f5fbf5e41591edfec3cf27002ff2190048464c4a
Zach <me@zackify.net>
2011-01-16 15:44:47 -0800

    Fixed title

--- login/index.php
+++ login/index.php
@@ -8,7 +8,7 @@ header("Location: ../index.php");
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Carbon Fiber Signup Form | Tutorialzine Demo</title>
+<title>Notepress - Login</title>
 
 <link rel="stylesheet" type="text/css" href="styles.css" />
 
